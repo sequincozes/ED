ba8441954ed756bcb02f655dfb03898ab84aac2d
Silvio <sequincozes@gmail.com>
2021-10-25 09:51:07 -0300

    Aula 6

---
+++ src/main/java/aula06/ManipuladorDeArquivos.java
@@ -0,0 +1,71 @@
+package aula06;
+
+import java.io.*;
+
+public class ManipuladorDeArquivos {
+
+    public File inicializa() {
+        File arquivo = new File("C:\\Users\\Silvio\\meusarquivos\\nome_do_arquivo.txt");
+        //verifica se o arquivo ou diretório existe
+        boolean existe = arquivo.exists();
+
+        if (!existe) {
+            //cria um arquivo (vazio)
+            try {
+                arquivo.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
+        return arquivo;
+    }
+
+    public void escreve(File arquivo, String texto, boolean sobrescrever) {
+        //construtor que recebe o objeto do tipo arquivo
+        try {
+            FileWriter fw = new FileWriter(arquivo, !sobrescrever);
+            //construtor recebe como argumento o objeto do tipo FileWriter
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            //escreve o conteúdo no arquivo
+            bw.write(texto);
+
+            //quebra de linha
+            bw.newLine();
+
+            //fecha os recursos
+            bw.close();
+            fw.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    public void leia(File arquivo) {
+        //construtor que recebe o objeto do tipo arquivo
+        try {
+            FileReader fr = new FileReader(arquivo);
+
+            //construtor que recebe o objeto do tipo FileReader
+            BufferedReader br = new BufferedReader(fr);
+
+            //enquanto houver mais linhas
+            while (br.ready()) { //lê a proxima linha
+                String linha = br.readLine();          //faz algo com a linha
+                System.out.println("LEU: " + linha);
+            }
+
+            br.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/aula06/Principal.java
+++ src/main/java/aula06/Principal.java
@@ -1,21 +1,28 @@
 package aula06;
 
+import java.io.File;
+
 public class Principal {
     public static void main(String[] args) {
-
-        TADFracao f1 = new TADFracao(4,2);
-        TADFracao f2 = new TADFracao(12,7);
+        TADFracao f1 = new TADFracao(4, 2);
+        TADFracao f2 = new TADFracao(12, 7);
 
         System.out.println(f1.getValorReal());
         System.out.println(f2.getValorReal());
 
         TADFracao f3 = f1.multiplicaFracao(f1, f2);
 
-        System.out.println(f3.getNumerador() +
-                "/" + f3.getDenominador() +
-                "=" + f3.getValorReal());
+//        System.out.println(f3.getNumerador() +
+//                "/" + f3.getDenominador() +
+//                "=" + f3.getValorReal());
 
-
+        // Salvando o valor real de uma fração em arquivo
+        ManipuladorDeArquivos mp = new ManipuladorDeArquivos();
+        File arquivo = mp.inicializa();
+        String texto = String.valueOf(f3.getValorReal());
+        mp.escreve(arquivo, texto, true);
+        mp.escreve(arquivo, texto, false);
+        mp.leia(arquivo);
 
     }
 }
